1778ea47c20b43e2bbeb1dee33d5edf6593f7f95
t_aga <aga.lhj@link8tech.tw>
2021-10-13 14:42:47 +0800

    淡入淡出

--- js/script.js
+++ js/script.js
@@ -10,19 +10,35 @@ let hoverSwitch;
 let videoItemDom;
 let showTime;
 
+reversePlaySvga.addEventListener('click', (e) => {
+    if(e.target.checked) {
+        demoCanvas.classList.add('reverse')
+    } else {
+        demoCanvas.classList.remove('reverse')
+    }
+})
+
 const handleChangeSvga = (target) => {
     hoverSwitch = true;
 
     parser.load(`./image/${target.dataset.src}`, (videoItem) => {
         videoItemDom = videoItem;
+        console.log('videoItem', videoItem)
         
         player.setVideoItem(videoItemDom);
         player.clearsAfterStop = false; // 動畫結束後，SVGA不會消失
         if(reversePlaySvga.checked) {
+            player.setVideoItem(videoItemDom);
+            demoCanvas.classList.add('fade')
             player.clearsAfterStop = true
             player.startAnimation(true); // 動畫反轉
+            player.onFinished(() => {
+                demoCanvas.classList.remove('fade')
+                demoCanvas.classList.remove('fadeIn')
+            });
             return
         }
+        demoCanvas.classList.add('fadeIn')
         player.startAnimation(); // 動畫開始
 
         // 計算到第幾幀
@@ -32,6 +48,11 @@ const handleChangeSvga = (target) => {
         if (delayRemoveSvga.checked) {
             player.onFinished(() => {
                 setTimeout(() => {
+                    demoCanvas.classList.add('fadeOut')
+                }, 1000)
+                setTimeout(() => {
+                    demoCanvas.classList.remove('fadeOut')
+                    demoCanvas.classList.remove('fadeIn')
                     player.clear(); // 移除SVGA
                 }, 3000);
             });
@@ -42,13 +63,19 @@ const handleChangeSvga = (target) => {
 const handleHoverSvga = () => {
     if (hoverSwitch && !delayRemoveSvga.checked && !reversePlaySvga.checked) {
         player.pauseAnimation();
+        demoCanvas.classList.add('fadeOut')
         player.startAnimationWithRange(
             { location: 0, length: showTime - 1 },
             true
         ); // 範圍 {location 初始幀，length 結束幀}， true(動畫反轉)，false(動畫正轉)
-        player.clearsAfterStop = true; // 動畫結束後，SVGA消失
-        hoverSwitch = false;
+        player.onFinished(() => {
+            demoCanvas.classList.remove('fadeOut')
+            demoCanvas.classList.remove('fadeIn')
+            player.clearsAfterStop = true; // 動畫結束後，SVGA消失
+        });
     }
+    
+    hoverSwitch = false;
 };
 
 for (let i = 0; i < svgaButton.length; i++) {
